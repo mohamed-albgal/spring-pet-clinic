f36ddcc6637e50b95a0aad344f7f65ac2c313414
Mohamed <malbgal@gmail.com>
2020-12-07 22:09:37 -0800

    fixed import statements on package declarations;

--- pet-clinic-web/src/test/java/dev/malb/petclinic_web/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/dev/malb/petclinic_web/SpringPetClinicApplicationTests.java
@@ -2,8 +2,8 @@ package dev.malb.petclinic_web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import dev.malb.petclinic_web.SpringPetClinicApplication;
+@SpringBootTest(classes = SpringPetClinicApplication.class)
 class SpringPetClinicApplicationTests {
 
     @Test
